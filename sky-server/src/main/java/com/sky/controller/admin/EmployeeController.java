a0a46f8d76644bd074841523050ad5e566024688
Shu <a1192405835@gmail.com>
2024-09-06 23:33:55 +0100

    completed the function for editing employ's status

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -91,4 +91,28 @@ public class EmployeeController {
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+    @PostMapping("/status/{status}")
+    @ApiOperation(value = "启用或禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,long id){
+        log.info("启用或禁用员工账号:{},{}",status,id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
+    @GetMapping("/{id}")
+    @ApiOperation(value = "根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable long id){
+        log.info("根据id查询员工信息: {}",id);
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+
+    }
+    @PutMapping
+    @ApiOperation(value = "编辑员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("编辑员工信息:{}",employeeDTO);
+        employeeService.update(employeeDTO);
+
+        return Result.success();
+    }
+
 }
